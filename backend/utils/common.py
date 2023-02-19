891df3c1e47891fd5e5a24d31c47a8cab6e6c7d1
= <poxen12@hotmail.com>
2023-02-19 17:55:56 +0100

    Created function to create random unique id

---
+++ backend/utils/common.py
@@ -0,0 +1,22 @@
+from utils.constants import ID_LENGTH
+from database import database
+from random import randrange
+
+def create_id(table: str) -> int:
+    # Creating id
+    id_parts = []
+    opts = '0123456789'
+    for i in range(ID_LENGTH):
+        id_parts.append(opts[randrange(0, len(opts))])
+    
+    id = int(''.join(id_parts))
+
+    # Checking if id is unique
+    user = database.fetch_one(
+        f"SELECT id FROM {table} WHERE id = %s",
+        (id,)
+    )
+    if user:
+        return create_id()
+
+    return id
