7f429ac56e58c69c21747bda8bedbc548c6f4d03
= <poxen12@hotmail.com>
2023-02-19 16:56:39 +0100

    Setup basic database class

--- backend/blueprints/users.py
+++ backend/blueprints/users.py
@@ -1,7 +1,14 @@
 from flask import Blueprint, jsonify
+from database import database
 
 users = Blueprint('users', __name__)
 
 @users.get('/users/<int:user_id>')
 def get_user_route(user_id: int):
+    connection, cursor = database.connection()
+
+    cursor.execute('SELECT * FROM users')
+    for user in cursor:
+        print(user)
+
     return jsonify({ 'user_id': user_id })
---
+++ backend/database.py
@@ -0,0 +1,54 @@
+import os, mysql.connector
+from mysql.connector.errors import DatabaseError
+from mysql.connector import pooling
+from mysql.connector.pooling import MySQLConnectionPool, PooledMySQLConnection
+from mysql.connector.cursor import MySQLCursor
+from typing import Tuple
+
+MYSQL_DATABASE = os.getenv('MYSQL_DATABASE')
+db_config = {
+    'host': os.getenv('MYSQL_HOST'),
+    'user': os.getenv('MYSQL_USER'),
+    'password': os.getenv('MYSQL_PASSWORD'),
+    'database': os.getenv('MYSQL_DATABASE')
+}
+
+POOL_SIZE = 10
+POOL_RESET_SESSION = True
+class Database():
+    def __init__(self):
+        self.pool = self.__create_connection()
+
+    def __create_connection(self) -> MySQLConnectionPool:
+        try:
+            return pooling.MySQLConnectionPool(
+                pool_name='pynative_pool',
+                pool_size=POOL_SIZE,
+                pool_reset_session=POOL_RESET_SESSION,
+                **db_config
+            )
+        except DatabaseError as e:
+            # Database does not exist
+            db = mysql.connector.connect(
+                host=db_config['host'],
+                user=db_config['user'],
+                passwd=db_config['password']
+            )
+
+            cursor = db.cursor()
+
+            # Creating database
+            cursor.execute(f'CREATE DATABASE IF NOT EXISTS {MYSQL_DATABASE}')
+            cursor.execute(F'USE {MYSQL_DATABASE}')
+
+            # Creating database tables
+            cursor.execute(f'CREATE TABLE IF NOT EXISTS users (id BIGINT(20), username VARCHAR(255), password VARCHAR(255), email VARCHAR(255) DEFAULT NULL, name VARCHAR(255) DEFAULT NULL, avatar VARCHAR(255) DEFAULT NULL, created_at BIGINT(20))')
+
+            return self.__create_connection()
+
+    def connection(self) -> Tuple[PooledMySQLConnection, MySQLCursor]:
+        connection = self.pool.get_connection()
+        cursor = connection._cnx.cursor(dictionary=True, buffered=True)
+        return connection, cursor
+
+database = Database()
