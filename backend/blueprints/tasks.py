a855140eb6ad937c875815b054d9a5c3cdc51071
= <poxen12@hotmail.com>
2023-02-21 17:31:51 +0100

    Added ability to remove assignee from task

--- backend/blueprints/tasks.py
+++ backend/blueprints/tasks.py
@@ -39,3 +39,33 @@ def add_assignee_route(task_id: int, assignee_id: int, token_id: int):
     created_assignee = get_assignee(assignee_id, task_id)
 
     return jsonify(created_assignee)
+
+@tasks.delete('/tasks/<int:task_id>/assignees/<int:assignee_id>')
+@token_required
+def remove_assignee_route(task_id: int, assignee_id: int, token_id: int):
+    # Checking if task exists
+    task = get_task_by_id(task_id)
+    if not task:
+        return 'Task not found', 404
+
+    # Checking if user is part of team
+    member = get_member(token_id, task['team_id'])
+    if not member:
+        return 'Unauthorized', 401
+
+    # Checking if assignee is available
+    assignee_member = get_member(assignee_id, task['team_id'])
+    if not assignee_member:
+        return 'Member not found', 404
+
+    # Checking if assignee is not already assigned task
+    assignee = get_assignee(assignee_id, task_id)
+    if not assignee:
+        return 'Member is not assigned this task', 409
+
+    # Unassigning assignee
+    query = "DELETE FROM assignees WHERE id = %s AND task_id = %s"
+    values = (assignee_id, task_id)
+    database.delete(query, values)
+
+    return jsonify({})
