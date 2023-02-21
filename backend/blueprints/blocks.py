25cb33393cee4d32eb1cbd68da7b3f73ef0222d2
= <poxen12@hotmail.com>
2023-02-21 17:11:04 +0100

    Added endpoint to fetch block tasks

--- backend/blueprints/blocks.py
+++ backend/blueprints/blocks.py
@@ -46,6 +46,33 @@ def add_group_task_route(block_id: int, token_id: int):
 
     return jsonify(task)
 
+@blocks.get('/blocks/<int:block_id>/tasks')
+@token_required
+def get_block_tasks_route(block_id: int, token_id: int):
+    # Checking if block exists
+    block = get_block_by_id(block_id)
+    if not block:
+        return 'Block not found', 404
+
+    # Checking if user is part of team
+    member = get_member(token_id, block['team_id'])
+    if not member:
+        return 'Unauthorized', 401
+
+    # Fetching task ids
+    query = "SELECT id FROM tasks WHERE block_id = %s"
+    values = (block_id,)
+    task_ids = database.fetch_many(query, values)
+
+    # Fetching task specific data
+    tasks = []
+    for task_id in task_ids:
+        task = get_task_by_id(task_id['id'])
+        if task:
+            tasks.append(task)
+    
+    return jsonify(tasks)
+
 @blocks.get('/blocks/<int:block_id>/tasks/<int:task_id>')
 @token_required
 def get_group_task_route(block_id: int, task_id: int, token_id: int):
