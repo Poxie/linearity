b35033aa21e2e2f5d5f82e22cf4de710d3e0bbf3
= <poxen12@hotmail.com>
2023-02-23 18:41:53 +0100

    Added frontend origin cors

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,6 @@
+import os
 from flask import Flask
+from flask_cors import CORS
 from blueprints.users import users
 from blueprints.teams import teams
 from blueprints.groups import groups
@@ -6,6 +8,8 @@ from blueprints.blocks import blocks
 from blueprints.tasks import tasks
 
 app = Flask(__name__)
+
+CORS(app, resources={r'/*': {'origins': os.getenv('FRONTEND_ORIGIN')}})
 
 app.register_blueprint(users)
 app.register_blueprint(teams)
