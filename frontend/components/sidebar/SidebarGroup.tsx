4d0c83101a1dda41ee2c98df825f7f0d5ac5afa3
= <poxen12@hotmail.com>
2023-02-23 22:52:33 +0100

    Made sidebar groups collapsable

--- frontend/components/sidebar/SidebarGroup.tsx
+++ frontend/components/sidebar/SidebarGroup.tsx
@@ -1,15 +1,27 @@
+import { useState } from 'react';
 import styles from './Sidebar.module.scss';
 
 export const SidebarGroup: React.FC<{
     header: string;
     children: any;
 }> = ({ header, children }) => {
+    const [expanded, setExpanded] = useState(true);
+
+    const toggleExpanded = () => setExpanded(!expanded);
+
+    const className = [
+        styles['group'],
+        !expanded ? styles['collapsed'] : ''
+    ].join(' ');
     return(
-        <div className={styles['group']}>
-            <button className={styles['group-header']}>
+        <div className={className}>
+            <button 
+                className={styles['group-header']}
+                onClick={toggleExpanded}
+            >
                 {header}
             </button>
-            {children}
+            {expanded && children}
         </div>
     )
 }
