3f062f80121e6aa953d6f8a824a3807fef2fcf39
= <poxen12@hotmail.com>
2023-02-23 21:13:31 +0100

    Added http request get method

--- frontend/contexts/auth/index.tsx
+++ frontend/contexts/auth/index.tsx
@@ -1,13 +1,18 @@
 "use client";
 
-import React, { ReactNode, useEffect, useState } from 'react';
+import React, { ReactNode, useCallback, useEffect, useState } from 'react';
 import { User } from '@/types';
 
-const AuthContext = React.createContext({} as {
+export type RequestMethod = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE';
+type AuthContextType = {
     profile: User | null;
     setToken: (token: string) => void;
+    token: string | null;
     loading: boolean;
-});
+    get: <T>(query: string, signal?: AbortSignal) => Promise<T>;
+}
+
+const AuthContext = React.createContext({} as AuthContextType);
 
 export const useAuth = () => React.useContext(AuthContext);
 
@@ -50,10 +55,59 @@ export default function AuthProvider({
             })
     }, [token]);
 
+    // Function to make http request
+    const makeRequest = useCallback(async function<T>(query: string, method: RequestMethod, body?: Object, signal?: AbortSignal) {
+        // If body is provided, create a form data
+        const formData = new FormData();
+        Object.entries(body || {}).forEach(([key, value]) => {
+            if(Array.isArray(value)) {
+                if(value[0] instanceof File) {
+                    value.forEach(v => formData.append(key, v));
+                } else {
+                    formData.append(key, JSON.stringify(value));
+                }
+                return;
+            }
+            formData.append(key, value);
+        })
+
+        // Creating request
+        return fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}${query}`, {
+            method,
+            headers: {
+                'Authorization': `Bearer ${token}`
+            },
+            body: method !== 'GET' ? formData : undefined,
+            signal
+        })
+        .then(async data => {
+            // Fetch failed for other reason
+            if(!data.ok) {
+                const error: any = new Error(await data.text());
+                error.code = data.status;
+
+                throw error as {
+                    message: string;
+                    code: number;
+                }
+            }
+
+            // Request is successful
+            return await data.json() as T;
+        })
+    }, [token]);
+
+    // Function to fetch data from API with user authentication.
+    const get = useCallback(async function<T>(query: string, signal?: AbortSignal) {
+        return makeRequest<T>(query, 'GET', {}, signal);
+    }, [token]);
+
     const value = {
         profile,
         setToken,
-        loading
+        get,
+        token,
+        loading,
     }
     return(
         <AuthContext.Provider value={value}>
