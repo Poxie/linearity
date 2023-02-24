3674ba3e2208c0d4e5dadaa035dd125449d54874
= <poxen12@hotmail.com>
2023-02-24 02:38:57 +0100

    Added the dropdown component

---
+++ frontend/components/dropdown/index.tsx
@@ -0,0 +1,74 @@
+import { useEffect, useRef, useState } from 'react';
+import { Input } from '../input';
+import styles from './Dropdown.module.scss';
+
+export const Dropdown: React.FC<{
+    items: {id: number, text: string}[];
+    onChange: (id: number) => void;
+    defaultSelected?: number;
+    allowSearch?: boolean;
+    resultsPosition?: 'left' | 'center' | 'right';
+}> = ({ items, onChange, defaultSelected, allowSearch, resultsPosition='left' }) => {
+    const [selected, setSelected] = useState(defaultSelected || items[0].id);
+    const [search, setSearch] = useState('');
+    const [open, setOpen] = useState(false);
+    const ref = useRef<HTMLDivElement>(null);
+
+    // Selecting new item
+    const selectItem = (id: number) => {
+        setSelected(id);
+        onChange(id);
+        setOpen(false);
+        setSearch('');
+    }
+
+    // Closing on click outside component
+    const checkForClickOutside = (e: MouseEvent) => {
+        // @ts-ignore: this works
+        if(ref.current && !ref.current.contains(e.target)) {
+            setOpen(false);
+        }
+    }
+    useEffect(() => {
+        document.addEventListener('mousedown', checkForClickOutside);
+        return () => document.removeEventListener('mousedown', checkForClickOutside);
+    }, []);
+
+    const selectableItems = !search ? items : items.filter(item => item.text.toLowerCase().includes(search.toLowerCase()));
+    const selectedItem = items.find(item => item.id === selected);
+
+    const className = [
+        styles['container'],
+        styles[resultsPosition]
+    ].join(' ')
+    return(
+        <div className={className} ref={ref}>
+            <button 
+                className={styles['selected']}
+                onClick={() => setOpen(!open)}
+            >
+                {selectedItem?.text}
+            </button>
+
+            {open && (
+                <ul>
+                    {allowSearch && (
+                        <Input 
+                            placeholder={'Search'}
+                            name="search"
+                            containerClassName={styles['search']}
+                            onChange={setSearch}
+                        />
+                    )}
+                    {selectableItems.map(item => (
+                        <li key={item.id}>
+                            <button onClick={() => selectItem(item.id)}>
+                                {item.text}
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
+    )
+}
