bfa9fe3797361949a0f515c2db84fd238eeaa196
= <poxen12@hotmail.com>
2023-03-02 00:09:06 +0100

    Added the due_at task attribute

--- backend/utils/constants.py
+++ backend/utils/constants.py
@@ -2,4 +2,4 @@ ID_LENGTH=14
 PATCH_USER_ALLOWED_PROPERTIES=['name']
 PATCH_TEAM_ALLOWED_PROPERTIES=['name', 'description']
 PATCH_GROUP_ALLOWED_PROPERTIES=['name', 'description']
-PATCH_TASK_ALLOWED_PROPERTIES=['title', 'description', 'block_id', 'position']
+PATCH_TASK_ALLOWED_PROPERTIES=['title', 'description', 'block_id', 'position', 'due_at']
