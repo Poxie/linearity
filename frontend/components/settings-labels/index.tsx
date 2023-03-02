ad9b8036cc9eeccf2615264873ad24079ba634ef
= <poxen12@hotmail.com>
2023-03-02 19:14:31 +0100

    Added create label button

--- frontend/components/settings-labels/index.tsx
+++ frontend/components/settings-labels/index.tsx
@@ -9,25 +9,40 @@ import { useAppSelector } from '@/redux/store';
 import { selectTeamLabels } from '@/redux/teams/selectors';
 import { LabelList } from '../label-list/LabelList';
 import { useState } from 'react';
+import Button from '../button';
+import { useModal } from '@/contexts/modal';
+import { AddLabelModal } from '@/modals/add-label/AddLabelModal';
 
 export const SettingsLabels: React.FC<{
     params: { teamId: string };
 }> = ({ params: { teamId } }) => {
+    const { setModal } = useModal();
+
     const labels = useAppSelector(state => selectTeamLabels(state, parseInt(teamId)));
     const [search, setSearch] = useState('');
+
+    const openModal = () => setModal(<AddLabelModal teamId={parseInt(teamId)} />);
 
     const filteredLabels = labels.filter(label => label.name.toLowerCase().includes(search.toLowerCase()));
     return(
         <div className={styles['container']}>
             <ModalGroup header={'Labels'} icon={<LabelIcon />}>
-                <Input 
-                    placeholder={'Search for label'}
-                    inputClassName={styles['input']}
-                    containerClassName={styles['input-container']}
-                    onChange={setSearch}
-                    name={'label-search'}
-                    icon={<SearchIcon />}
-                />
+                <div className={styles['header']}>
+                    <Input 
+                        placeholder={'Search for label'}
+                        inputClassName={styles['input']}
+                        containerClassName={styles['input-container']}
+                        onChange={setSearch}
+                        name={'label-search'}
+                        icon={<SearchIcon />}
+                    />
+                    <Button 
+                        className={styles['button']}
+                        onClick={openModal}
+                    >
+                        Create label
+                    </Button>
+                </div>
 
                 {!filteredLabels.length && (
                     <span className={styles['empty']}>	
