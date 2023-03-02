e5165556262f77dd354525a8207f4d157ec4f0c4
= <poxen12@hotmail.com>
2023-03-02 03:33:08 +0100

    Open context menu on block header context menu

--- frontend/components/group/GroupBlockHeader.tsx
+++ frontend/components/group/GroupBlockHeader.tsx
@@ -2,20 +2,53 @@ import styles from './Group.module.scss';
 import { useAppSelector } from "@/redux/store";
 import { selectBlockInfo, selectBlockTaskCount } from "@/redux/teams/selectors";
 import { useBlock } from "./GroupBlock"
+import { MenuGroup, useMenu } from '@/contexts/menu';
+import { useRef } from 'react';
+import { useModal } from '@/contexts/modal';
+import { EditBlockModal } from '@/modals/edit-block/EditBlockModal';
+import { AddTaskModal } from '@/modals/add-task/AddTaskModal';
 
 export const GroupBlockHeader: React.FC<{
     onMouseUp: () => void;
     onMouseDown: () => void;
 }> = ({ onMouseDown, onMouseUp }) => {
-    const { blockId } = useBlock();
+    const { setMenu } = useMenu();
+    const { setModal } = useModal();
+    const { blockId, groupId } = useBlock();
+
     const { name } = useAppSelector(state => selectBlockInfo(state, blockId));
     const taskCount = useAppSelector(state => selectBlockTaskCount(state, blockId));
+
+    const ref = useRef<HTMLDivElement>(null);
+
+    const addIssue = () => {
+        setModal(<AddTaskModal blockId={blockId} />);
+    }
+    const editBlock = () => {
+        setModal(<EditBlockModal groupId={groupId} blockId={blockId} />)
+    }
+    const openMenu = (e: React.MouseEvent) => {
+        const groups: MenuGroup[] = [
+            [
+                { text: 'Edit block', onClick: editBlock, type: 'default' },
+                { text: 'Add issue', onClick: addIssue, type: 'default' }
+            ]
+        ]
+
+        setMenu({
+            groups,
+            event: e,
+            element: ref,
+        })
+    }
 
     return(
         <div 
             className={styles['block-header']}
             onMouseDown={onMouseDown}
             onMouseUp={onMouseUp}
+            onContextMenu={openMenu}
+            ref={ref}
         >
             <div className={styles['block-header-main']}>
                 <span className={styles['block-title']}>
