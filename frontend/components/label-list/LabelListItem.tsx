c396ef378e440cf9dc7e25934cecea6c52b87c7a
= <poxen12@hotmail.com>
2023-03-02 20:19:14 +0100

    Showing edit modal on edit label context menu button

--- frontend/components/label-list/LabelListItem.tsx
+++ frontend/components/label-list/LabelListItem.tsx
@@ -4,6 +4,8 @@ import { MenuGroup, useMenu } from '@/contexts/menu';
 import { useRef } from 'react';
 import { useSettings } from '../settings';
 import { useTeam } from '@/hooks/useTeam';
+import { useModal } from '@/contexts/modal';
+import { AddLabelModal } from '@/modals/add-label/AddLabelModal';
 
 export const LabelListItem: React.FC<{
     label: Label;
@@ -11,6 +13,7 @@ export const LabelListItem: React.FC<{
     onLabelClick?: (label: Label) => void;
 }> = ({ label, hasContextMenu, onLabelClick }) => {
     const { setMenu } = useMenu();
+    const { setModal } = useModal();
     const { removeLabel } = useTeam(label.team_id);
 
     const ref = useRef<HTMLButtonElement>(null);
@@ -20,7 +23,7 @@ export const LabelListItem: React.FC<{
 
         const groups: MenuGroup[] = [
             [
-                { text: 'Edit label', onClick: () => {}, type: 'default' }
+                { text: 'Edit label', onClick: () => setModal(<AddLabelModal teamId={label.team_id} label={label} />), type: 'default' }
             ],
             [
                 { text: 'Delete label', onClick: () => removeLabel(label), type: 'danger' }
