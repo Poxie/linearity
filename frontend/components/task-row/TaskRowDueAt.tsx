cfa16406bfa758cb2c14899bb090073118505d81
= <poxen12@hotmail.com>
2023-03-03 02:28:23 +0100

    Added ability to update task row due at

--- frontend/components/task-row/TaskRowDueAt.tsx
+++ frontend/components/task-row/TaskRowDueAt.tsx
@@ -2,14 +2,19 @@ import styles from './TaskRow.module.scss';
 import { useAppSelector } from "@/redux/store"
 import { selectTaskInfo } from "@/redux/teams/selectors"
 import { TimeSeletor } from "../time-selector"
+import { useTask } from '@/hooks/useTask';
 
 export const TaskRowDueAt: React.FC<{
     taskId: number;
 }> = ({ taskId }) => {
     const { due_at } = useAppSelector(state => selectTaskInfo(state, taskId));
+    const { updateProperty } = useTask(taskId);
+
+    const updateDueAt = (date: Date | null) => updateProperty('due_at', date?.getTime(), due_at);
+
     return(
         <TimeSeletor 
-            onChange={console.log}
+            onChange={updateDueAt}
             defaultTime={due_at}
             emptyLabel={'Due date not set'}
             popoutPosition={'left'}
