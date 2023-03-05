33fc2aa94b4c07d95c2ff169bcb33b630bd75bc8
= <poxen12@hotmail.com>
2023-03-05 16:36:16 +0100

    Using label list component

--- frontend/components/settings/SettingsLabelList.tsx
+++ frontend/components/settings/SettingsLabelList.tsx
@@ -1,36 +1,23 @@
-import styles from './Settings.module.scss';
-import { AddIcon } from "@/assets/icons/AddIcon";
+import { useModal } from "@/contexts/modal";
+import { AddLabelModal } from "@/modals/add-label/AddLabelModal";
 import { useAppSelector } from "@/redux/store";
 import { selectTeamLabels } from "@/redux/teams/selectors";
 import { useSettings } from "."
 import { LabelList } from "../label-list/LabelList";
-import { useModal } from '@/contexts/modal';
-import { AddLabelModal } from '@/modals/add-label/AddLabelModal';
 
 export const SettingsLabelList = () => {
-    const { teamId } = useSettings();
     const { setModal } = useModal();
+    const { teamId } = useSettings();
     const labels = useAppSelector(state => selectTeamLabels(state, teamId));
 
-    const openModal = () => {
-        setModal(<AddLabelModal teamId={teamId} />);
-    }
+    const openModal = () => setModal(<AddLabelModal teamId={teamId} />);
 
     return(
-        <div className={styles['labels']}>
-            {labels.length !== 0 && (
-                <LabelList 
-                    labels={labels}
-                    hasContextMenu
-                    contextMenuOnClick
-                />
-            )}
-            <button 
-                className={styles['add-label-button']}
-                onClick={openModal}
-            >
-                <AddIcon />
-            </button>
-        </div>
+        <LabelList 
+            labels={labels}
+            hasContextMenu
+            contextMenuOnClick
+            onAddButtonClicked={openModal}
+        />
     )
 }
