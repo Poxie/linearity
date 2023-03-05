1a6bb7cbf1137cfef6be78c764583f1e8cd2f64c
= <poxen12@hotmail.com>
2023-03-05 16:29:57 +0100

    Using editable text component for team title and description

--- frontend/components/settings/index.tsx
+++ frontend/components/settings/index.tsx
@@ -6,13 +6,12 @@ import { LabelIcon } from '@/assets/icons/LabelIcon';
 import { useTeam } from '@/hooks/useTeam';
 import { ModalGroup } from '@/modals/ModalGroup';
 import { useAppSelector } from '@/redux/store';
-import { selectTeamById, selectTeamLabels } from '@/redux/teams/selectors';
-import { useRef, useState } from 'react';
-import { Input } from '../input';
+import { selectTeamById } from '@/redux/teams/selectors';
 import styles from './Settings.module.scss';
 import { MemberIcon } from '@/assets/icons/MemberIcon';
 import { SettingsLabelList } from './SettingsLabelList';
 import { SettingsMemberList } from './SettingsMemberList';
+import { EditableText } from '../editable-text';
 
 const SettingsContext = React.createContext({} as {
     teamId: number;
@@ -24,61 +23,30 @@ export const Settings = ({ params: { teamId } }: {
     params: { teamId: string }
 }) => {
     const team = useAppSelector(state => selectTeamById(state, parseInt(teamId)));
-    const labels = useAppSelector(state => selectTeamLabels(state, parseInt(teamId)));
     const { updateProperty } = useTeam(parseInt(teamId));
-
-    const [nameEdit, setNameEdit] = useState(false);
-    const [descriptionEdit, setDescriptionEdit] = useState(false);
-
-    const nameRef = useRef<HTMLInputElement>(null);
-    const descriptionRef = useRef<HTMLInputElement>(null);
 
     if(!team) return null;
 
-    const updateName = () => {
-        if(!nameRef.current?.value || name === nameRef.current?.value) return setNameEdit(false);
-        updateProperty('name', nameRef.current?.value, name);
-        setNameEdit(false);
-    }
-    const updateDescription = () => {
-        if(description === descriptionRef.current?.value) return setDescriptionEdit(false);
-        updateProperty('description', descriptionRef.current?.value, description);
-        setDescriptionEdit(false);
-    }
-    
     const { name, description } = team;
+    
+    const updateName = (text: string) => updateProperty('name', text, name)
+    const updateDescription = (text: string) => updateProperty('description', text, description);
     return(
         <SettingsContext.Provider value={{ teamId: parseInt(teamId) }}>
             <div className={styles['container']}>
                 <ModalGroup header={'Team information'} icon={<InfoIcon />}>
                     <div className={styles['header']}>
-                        {!nameEdit ? (
-                            <h1 onClick={() => setNameEdit(true)}>
-                                {name}
-                            </h1>
-                        ) : (
-                            <Input 
-                                placeholder={'Name'}
-                                defaultValue={name}
-                                onBlur={updateName}
-                                ref={nameRef}
-                                focusOnMount
-                            />
-                        )}
-                        {!descriptionEdit && description ? (
-                            <span onClick={() => setDescriptionEdit(true)}>
-                                {description}
-                            </span>
-                        ) : (
-                            <Input 
-                                placeholder={'Add a better description'}
-                                defaultValue={description || ''}
-                                onBlur={updateDescription}
-                                ref={descriptionRef}
-                                focusOnMount
-                                textArea
-                            />
-                        )}
+                        <EditableText 
+                            placeholder={'Name'}
+                            defaultValue={name}
+                            onChange={updateName}
+                            size={'large'}
+                        />
+                        <EditableText 
+                            placeholder={'Add a better description'}
+                            defaultValue={description || ''}
+                            onChange={updateDescription}
+                        />
                     </div>
                 </ModalGroup>
                 <ModalGroup header={'Team labels'} icon={<LabelIcon />}>
