e15177995adb7cd7fb27782f1a279597a2b5cb34
= <poxen12@hotmail.com>
2023-03-05 05:10:06 +0100

    Added no due date notice

--- frontend/components/group/GroupTaskDueAt.tsx
+++ frontend/components/group/GroupTaskDueAt.tsx
@@ -16,25 +16,27 @@ export const GroupTaskDueAt: React.FC<{
     const { due_at } = useAppSelector(state => selectTaskInfo(state, taskId));
     const ref = useRef<HTMLButtonElement>(null);
 
-    if(!due_at) return <div></div>;
-
     const openPopout = () => setPopout({
         popout: <TimeSelectorPopout onChange={date => updateProperty('due_at', date?.getTime(), due_at)} closeOnSelect />,
         ref
     })
 
-    const date = new Date(due_at);
-    const readableDate = date.toLocaleString('en', { dateStyle: 'medium' }).split(',')[0];
-    const readableLongDate = date.toLocaleString('en', { dateStyle: 'long' });
+    let readableDate: string | undefined;
+    let readableLongDate: string | undefined;
+    if(due_at) {
+        const date = new Date(due_at);
+        readableDate = date.toLocaleString('en', { dateStyle: 'medium' }).split(',')[0];
+        readableLongDate = date.toLocaleString('en', { dateStyle: 'long' });
+    }
     return(
-        <HasTooltip tooltip={`Due at ${readableLongDate}`}>
+        <HasTooltip tooltip={readableDate ? `Due at ${readableLongDate}` : 'Set due date'}>
             <button 
                 className={styles['due-at']}
                 onClick={openPopout}
                 ref={ref}
             >
                 <TimeIcon />
-                {readableDate}
+                {readableDate || 'No due date'}
             </button>
         </HasTooltip>
     )
