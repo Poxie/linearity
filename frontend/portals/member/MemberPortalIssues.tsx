3a7629193d0ea8bdc0335e9d29d526fc0743b3bb
= <poxen12@hotmail.com>
2023-03-06 22:15:08 +0100

    Removed previous member portal tasks fetching

--- frontend/portals/member/MemberPortalIssues.tsx
+++ frontend/portals/member/MemberPortalIssues.tsx
@@ -1,30 +1,14 @@
 import styles from './MemberPortal.module.scss';
 import { GroupTask } from "@/components/group/GroupTask";
-import { useAuth } from "@/contexts/auth";
-import { useAppDispatch, useAppSelector } from "@/redux/store";
-import { setTasks } from "@/redux/teams/actions";
+import { useAppSelector } from "@/redux/store";
 import { selectMemberById, selectTasksByAssignee } from "@/redux/teams/selectors";
-import { Task } from "@/types";
-import { useEffect, useState } from "react";
 
 export const MemberPortalIsseus: React.FC<{
     teamId: number;
     memberId: number;
 }> = ({ teamId, memberId }) => {
-    const { get } = useAuth();
-    const dispatch = useAppDispatch();
-
     const tasks = useAppSelector(state => selectTasksByAssignee(state, teamId, memberId));
     const member = useAppSelector(state => selectMemberById(state, teamId, memberId));
-    
-    useEffect(() => {
-        if(tasks.length) return;
-
-        get<Task[]>(`/teams/${teamId}/members/${memberId}/tasks`)
-            .then(tasks => {
-                dispatch(setTasks(tasks));
-            })
-    }, [tasks.length, teamId, memberId]);
 
     return(
         <div className={styles['tasks']}>
